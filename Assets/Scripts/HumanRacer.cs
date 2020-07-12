232122bbf1633d4c2b3e958cd533c31fc5d27b7d
mevlanaayas <mevlanaayas@gmail.com>
2020-07-12 13:51:23 +0300

    added: current position at the race

---
+++ Assets/Scripts/HumanRacer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DrawRace
+{
+    public class HumanRacer : MonoBehaviour
+    {
+        private Vector3 _position;
+
+        private void Start()
+        {
+            _position = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            _position = transform.position;
+        }
+
+        public Vector3 Position()
+        {
+            return _position;
+        }
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DrawRace
+{
+    public class World : MonoBehaviour
+    {
+
+        public Text raceStatus;
+        public Text minimap;
+
+        private HumanRacer _humanRacer;
+        private AiRacer _aiRacer;
+
+        private void Start()
+        {
+            _humanRacer = FindObjectOfType<HumanRacer>();
+            _aiRacer = FindObjectOfType<AiRacer>();
+        }
+
+        private void Update()
+        {
+            if (_humanRacer.Position().x >= _aiRacer.Position().x)
+            {
+                raceStatus.text = "1";
+            }
+            else
+            {
+                raceStatus.text = "2";
+            }
+        }
+    }
+}
